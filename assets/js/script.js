5f3e6ce086544771db7f133fbc0ecf1f57f9ace3
Code-Monster1 <usupovaliakbar01@gmail.com>
2022-12-05 15:23:27 +0600

    project full-done

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,26 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    440: {
+      slidesPerView: 2,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    720: {
+      slidesPerView: 3,
+    }
+  }
 });
